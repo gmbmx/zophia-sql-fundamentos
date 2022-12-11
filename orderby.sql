9763f3202b14a5f2bae973b1cbabb5e6b33a06f7
Guillermo <101653528+gmbmx@users.noreply.github.com>
2022-12-11 10:33:05 -0600

    Add order by, fix comment style

---
+++ orderby.sql
@@ -0,0 +1,69 @@
+-- El ordenamiento default es de menor a mayor
+SELECT *
+FROM empleados
+ORDER BY nombre;
+
+-- El tipo de dato es importante para el ordenamiento
+SELECT *
+FROM empleados
+ORDER BY antiguedad;
+
+-- Usamos DESC para ordenar de mayor a menor
+SELECT nombre
+FROM empleados
+ORDER BY nombre DESC;
+
+-- Usamos ASC para ordenar explícitamente de menor a mayor
+SELECT nombre
+FROM empleados
+ORDER BY nombre ASC;
+
+-- Para ordenar por múltiples campos hay que separarlos con coma
+SELECT nombre, 
+    apellido,
+    departamento
+FROM empleados
+ORDER BY departamento,
+    nombre;
+
+-- El orden de los campos en ORDER BY determina cómo lo hará
+SELECT nombre, 
+    apellido,
+    departamento
+FROM empleados
+ORDER BY nombre,
+    departamento;
+
+--Podemos combinar el ordenamiento por columnas
+SELECT nombre, 
+    apellido,
+    departamento
+FROM empleados
+ORDER BY departamento DESC,
+    nombre; 
+
+--Otro ejemplo que mezcla la manera de ordenar
+SELECT nombre, 
+    apellido,
+    departamento
+FROM empleados
+ORDER BY departamento,
+    nombre DESC;
+
+--Errores comunes
+
+--Orden de los statements
+SELECT *
+ORDER BY nombre
+FROM empleados;
+
+--Orden en DESC
+SELECT *
+FROM empleados
+ORDER BY DESC departamento;
+
+--Falta de comas
+SELECT *
+FROM empleados
+ORDER BY departamento 
+    DESC nombre;
--- select.sql
+++ select.sql
@@ -5,86 +5,94 @@ para comentarios
 de varias líneas
 */
 
-SELECT * FROM empleados;
 -- '*' es un caracter especial que nos permite seleccionar todas las columnas de la tabla
+SELECT * FROM empleados;
 
-select * from empleados;
 -- Esto funciona ya que SQL no es Case Sensitive
+select * from empleados;
 
-seLEct * FroM empleados;
 -- Sigue funcionando, SQL no es Case Sensitive
+seLEct * FroM empleados;
 
-SELECT * empleados;
 --Error, falta un FROM statement
+SELECT * empleados;
 
-SELECT * FROM empleado;
 --Error, nombre de tabla erróneo
+SELECT * FROM empleado;
 
-SELECT * FROM Empleados;
 -- Esto funciona pero no se recomienda
+SELECT * FROM Empleados;
 
-SELECT nombre FROM empleados;
 -- Podemos seleccionar sólo una columna
+SELECT nombre FROM empleados;
 
-SELECT nombre, apellido FROM empleados;
 -- Para seleccionar varias columnas hay que separarlas con comas
+SELECT nombre, apellido FROM empleados;
 
-SELECT apellido, nombre FROM empleados;
 -- El orden en el que solicitamos las columnas importa
+SELECT apellido, nombre FROM empleados;
 
-SELECT apellido nombre FROM empleados;
 --Error, hay que separar las columnas con comas
+SELECT apellido nombre FROM empleados;
 
-SELECT apllido, nombre FROM empleados;
 --Error, hay que validar que el nombre de las columnas sea correcto
+SELECT apllido, nombre FROM empleados;
 
-SELECT nombre
-    ,apellido_paterno
-    ,apellido_materno
-FROM empleados;
 -- Código limpio, leading comma
-
-SELECT 
-    nombre,
-    apellido_paterno,
-    apellido_materno
+SELECT nombre
+    ,apellido
+    ,pais
+    ,email
 FROM empleados;
+
 -- Código limpio, trailing comma
-
-SELECT nombre
-    --,apellido_paterno
-    ,apellido_materno
+SELECT 
+    nombre,
+    apellido,
+    pais,
+    email
 FROM empleados;
+
 -- Leading comma permite quitar columnas más fácil al modificar el query
-
-SELECT 
-    nombre,
-    --apellido_paterno,
-    apellido_materno
-FROM empleados;
--- Trailing comma también
-
-SELECT --nombre
-    ,apellido_paterno
-    ,apellido_materno
-FROM empleados;
--- Si usas leading comma sólo tendrás que tener cuidado si comentas la primera columna
-
-SELECT 
-    nombre,
-    apellido_paterno,
-    --apellido_materno
-FROM empleados;
--- Si usas trailing comma sólo tendrás que tener cuidado si comentas la última columna
-
 SELECT nombre
-    apellido_paterno
-    ,apellido_materno
+    --,apellido
+    ,pais
+    ,email
+FROM empleados;
+
+-- Trailing comma también
+SELECT 
+    nombre,
+    --apellido,
+    pais,
+    email
+FROM empleados;
+
+-- Si usas leading comma sólo tendrás que tener cuidado si comentas la primera columna
+SELECT --nombre
+    ,apellido
+    ,pais
+    ,email
+FROM empleados;
+
+-- Si usas trailing comma sólo tendrás que tener cuidado si comentas la última columna
+SELECT 
+    nombre,
+    apellido,
+    pais,
+    --email
+FROM empleados;
+
+--Para muchas personas es más sencillo ver que te falta una coma con trailing comma
+SELECT nombre
+    apellido
+    ,pais
+    ,email
 FROM empleados;
 
 SELECT 
     nombre,
-    apellido_paterno
-    apellido_materno
+    apellido
+    pais,
+    email
 FROM empleados;
---Para muchas personas es más sencillo ver que te falta una coma con trailing comma
